c51b1d2c8baec0e4732dd261efd83000c38067aa
devmunro <defmunro@gmail.com>
2024-04-14 19:43:06 +0100

    Update header image width in Header.css and refactor Header component in Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import {images} from '../../constants';
-import './Header.css';
-import { SubHeading } from '../../components';
+import React from "react";
+import { images } from "../../constants";
+import "./Header.css";
+import { SubHeading } from "../../components";
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
-    <div className='app__wrapper_info'>
-      <SubHeading title="Chase the new flavour"/>
-      <h1 className='app__header-h1'>Gastro Pub for the Senses</h1>
-      <p className='p__opensans' style={{margin: '2rem 0'}}>We are a gastro pub located in the heart of the city. We are the best in town and provide the best food and drinks for our customers.</p>
-  <button type='button' className='custom__button'>Explore Menu</button>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">Gastro Pub for the Senses</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        We are a gastro pub located in the heart of the city. We are the best in
+        town and provide the best food and drinks for our customers.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
-    <div className='app__header-img'>
-      <img src={images.gastro} alt='header' style={{border: '2px dashed gold', margin: '0 2rem'}}/>
+    <div className="app__header-img">
+      <img src={images.gastro} alt="header" style={{ margin: "1rem 2rem" }} />
     </div>
   </div>
 );
