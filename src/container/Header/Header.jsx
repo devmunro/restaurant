2c163ae57a0387be056a9134eb81773d899af56a
devmunro <defmunro@gmail.com>
2024-04-15 19:51:22 +0100

    Fix typo in Header component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">Savour Gastro Pub</h1>
+      <h1 className="app__header-h1">Savor Gastro Pub</h1>
       <p className="p__opensans" style={{ margin: "2rem 0" }}>
         We are a gastro pub located in the heart of the city. We are the best in
         town and provide the best food and drinks for our customers.
