5b1a24cfe359a22cfc5980f41540cb8de192b33b
devmunro <defmunro@gmail.com>
2024-04-17 20:48:07 +0100

    Refactor menu item styles and structure in MenuItem.css and MenuItem.jsx

--- src/constants/images.js
+++ src/constants/images.js
@@ -7,10 +7,8 @@ import gallery03 from '../assets/gallery03.png';
 import gallery04 from '../assets/gallery04.png';
 import knife from '../assets/knife.png';
 import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
 import findus from '../assets/findus.png';
 import laurels from '../assets/laurels.png';
 import award01 from '../assets/award01.png';
@@ -22,6 +20,8 @@ import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
 import savor from '../assets/savor.png';
 import gastro from '../assets/gastro.png';
+import craftbeer from '../assets/craftbeer.png';
+
 
 
 
@@ -35,10 +35,8 @@ export default {
   gallery04,
   knife,
   logo,
-  menu,
   overlaybg,
   spoon,
-  welcome,
   findus,
   laurels,
   award01,
@@ -50,4 +48,5 @@ export default {
   gericht,
   savor,
   gastro,
+  craftbeer,
 };
